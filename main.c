d2865f3b64f61832009235f13c8eb54e07073060
dnjyj1024 <dnjyj1024@naver.com>
2022-11-15 16:23:35 +0900

    01_realfinal

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int i;
+	int i=10;
 	char c = 'a';
 	int *iptr;
 	iptr = &i;

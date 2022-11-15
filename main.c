3463b6544ba656ebafcf7a4f2845154810e7495b
dnjyj1024 <dnjyj1024@naver.com>
2022-11-15 16:28:44 +0900

    02

--- main.c
+++ main.c
@@ -4,16 +4,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int i=10;
-	char c = 'a';
-	int *iptr;
-	iptr = &i;
-	char *cptr;
-	cptr = &c;
-	int *iptr2;
-	iptr2 = iptr;
-	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
-	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
-	printf("ptr2 : %p, %i\n", iptr2, *iptr2);
+	int i=300;
+	
+	int *pi = &i;
+	char *pc = &i;
+	
+	printf("%i, %i, %i\n", i, *pi, *pc);
 	return 0;
 }
